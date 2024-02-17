ae3a122ae1b397dacc05fbee7b1bc980d47a805f
Robostangs-Software <122229138+Robostangs-Software@users.noreply.github.com>
2024-02-17 13:58:26 -0500

    update constants + add additional mask

--- src/main/java/frc/robot/Vision/note_finder.py
+++ src/main/java/frc/robot/Vision/note_finder.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import time
 
-MIN_AREA = 500
+MIN_AREA = 10
 MAX_AREA = 10000
 MAX_RATIO = 0.7
 
@@ -18,14 +18,26 @@ def runPipeline(image, llrobot):
 
     # BGR->HSV
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # kSize = 10
+    # cv2.blur(image, (kSize, kSize), cv2.BORDER_DEFAULT)
 
     # mask the img
-    lower_bound = np.array([0, 130, 170])
-    upper_bound = np.array([25, 245, 255])
-    mask = cv2.inRange(hsv, lower_bound, upper_bound)
+    lower_bound1 = np.array([0, 130, 150])
+    upper_bound1 = np.array([10, 255, 255])
+    mask = cv2.inRange(hsv, lower_bound1, upper_bound1)
+
+    lower_bound2 = np.array([200, 130, 150])
+    upper_bound2 = np.array([255, 255, 255])
+    mask2 = cv2.inRange(hsv, lower_bound2, upper_bound2)
+    mask = (255*(mask+mask2)).clip(0, 255).astype("uint8")
 
     # find contours in the mask
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #contours = np.asarray(contours)
+
+    # print(type(contours))
+    #print([len(a) for a in contours])
+    cv2.drawContours(image, contours, -1, (0,255,0,100), 3)
 
     if len(contours) == 0: # cannot find any contours
         return np.array([[]]), image, llpython
@@ -52,9 +64,10 @@ def runPipeline(image, llrobot):
                 # Draw ellipse
                 cv2.ellipse(image, ellipse, (0, 255, 0), 2)
                 llpython = [x, y, w, h]
+                toc = time.perf_counter()
+                print("1 cycle ran in " + str(toc - tic) + " time")
                 return contour, image, llpython
 
     toc = time.perf_counter()
-    print("1 cycle ran in " + str(toc - tic) + " time")
-    
+    # print("1 cycle ran in " + str(toc - tic) + " time")
     return np.array([[]]), image, llpython # cannot find a contour that meets requirement

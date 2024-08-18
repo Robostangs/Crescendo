612e6d48130589ebf266dcbdea4c79acdb375f9e
Robostangs-Software <122229138+Robostangs-Software@users.noreply.github.com>
2024-08-18 11:26:57 -0400

    Q51 changed sitting on hardstop value, might not work, changed auto to wait after intake

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -419,7 +419,8 @@ public final class Constants {
 		public static final double kFeedForwardTorqueCurrentWhileShooting = 6;
 
 		public static final double shooterTrapezoidalOffset = 2.6;
-		public static final double angleOfShooterReferencePointSittingOnHardStop = -62.6;
+		public static final double angleOfShooterReferencePointSittingOnHardStop = -62.5;
+		
 
 		/**
 		 * Value that gets multiplied against the FineAdjust input variable, this number
@@ -529,7 +530,7 @@ public final class Constants {
 			public static final int kId = 7;
 		}
 
-		public static final double kDefaultStatorCurrentLimit = 55;
+		public static final double kDefaultStatorCurrentLimit = 120;
 		public static final double kHomingCurrentLimit = 12;
 		public static final double kHomingPower = -0.2;
 		public static final double kHardStopPositionRelativeToSwitchMeters = -0.02;
